12895f2529ed3b467dc65f842de33dad4e4adde0
Ruihong Zhou <ruzhou@ucdavis.edu>
2025-02-24 23:45:51 -0800

    every picture project - final

--- everypicture/v2-final/scripts.js
+++ everypicture/v2-final/scripts.js
@@ -10,33 +10,34 @@
         const modal = document.getElementById(modalId);
 
         overlay.addEventListener('click', (e) => {
-            // e.stopPropagation();
-            modal.style.display = 'block';
+            modal.classList.add('fade-in');
         });
         
         // close the modal by clicking the close button
         const closeButton = modal.querySelector('.close-modal');
         closeButton.addEventListener('click', () => {
-            modal.style.display = 'none';
+            modal.classList.remove('fade-in');
         });
 
         // close the modal by click anywhere outside of the modal
         modal.addEventListener('click', (e) => {
-            if (e.target === modal){
-                modal.style.display = 'none';
-            };
+            if (e.target === modal) {
+                modal.classList.remove('fade-in');
+            }
         });
+
+        
     });
 
     // close the modal by pressing ESC key
     document.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape'){
-            const modalOpened = document.querySelector('.modal[style="display: block;"]');
-            if (modalOpened){
-                modalOpened.style.display = 'none';
+        if (e.key === 'Escape') {
+            const modalOpened = document.querySelector('.modal.fade-in');
+            if (modalOpened) {
+                modalOpened.classList.remove('fade-in');
             }
         }
     });
-
+    
 })();
 
