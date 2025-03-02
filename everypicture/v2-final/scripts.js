7ca9c8df75ead5b9b12894ed0895d36632d88e23
Ruihong Zhou <ruzhou@ucdavis.edu>
2025-03-02 12:56:59 -0800

    Refactored code

--- everypicture/v2-final/scripts.js
+++ everypicture/v2-final/scripts.js
@@ -4,37 +4,36 @@
 
     const galleryItems = document.querySelectorAll('.gallery-item');
         
-    galleryItems.forEach((item, index) => {
+    for (let i = 0; i < galleryItems.length; i++) {
+        const item = galleryItems[i];
         const overlay = item.querySelector('.info-overlay');
-        const modalId = `modal-${index + 1}`;
-        const modal = document.getElementById(modalId);
+        const modalId = 'modal-' + (i + 1);
+        const modal = document.querySelector(`#${modalId}`);
 
-        overlay.addEventListener('click', (e) => {
-            modal.classList.add('fade-in');
+        overlay.addEventListener('click', function() {
+            modal.className = modal.className + ' fade-in';
         });
         
         // close the modal by clicking the close button
         const closeButton = modal.querySelector('.close-modal');
-        closeButton.addEventListener('click', () => {
-            modal.classList.remove('fade-in');
+        closeButton.addEventListener('click', function() {
+            modal.className = modal.className.replace(' fade-in', '');
         });
 
         // close the modal by click anywhere outside of the modal
-        modal.addEventListener('click', (e) => {
+        modal.addEventListener('click', function(e) {
             if (e.target === modal) {
-                modal.classList.remove('fade-in');
+                modal.className = modal.className.replace(' fade-in', '');
             }
         });
-
-        
-    });
+    }
 
     // close the modal by pressing ESC key
-    document.addEventListener('keydown', (e) => {
+    document.addEventListener('keydown', function(e) {
         if (e.key === 'Escape') {
             const modalOpened = document.querySelector('.modal.fade-in');
             if (modalOpened) {
-                modalOpened.classList.remove('fade-in');
+                modalOpened.className = modalOpened.className.replace(' fade-in', '');
             }
         }
     });
